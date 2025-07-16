09c418fee5a36a2f98ffe79fa798af5c3598fc52
Arsenicmeister <98180323+Arsenicmeister@users.noreply.github.com>
2025-07-16 13:22:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to SBI OpsVision 360</h1>
+      <p>This is your React frontend deployed with backend on Vercel!</p>
+    </div>
+  );
+}
+
+export default App;
