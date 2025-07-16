f791d79c680f9a9a270ae3afb977a5214929b90c
Arsenicmeister <98180323+Arsenicmeister@users.noreply.github.com>
2025-07-16 19:30:52 +0530

    Update supabaseClient.js

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://jixeqatzhrsqrbmdcvyk.supabase.co";
-const supabaseKey = "YOUR_PUBLIC_ANON_KEY";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeGVxYXR6aHJzcXJibWRjdnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1ODQ4NTQsImV4cCI6MjA2ODE2MDg1NH0.4mTKl9L3hyv40FolCnDDyEc1fdywuQMkWFKjXs4gWYA";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
