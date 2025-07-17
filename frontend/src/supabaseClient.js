25cef1e051121460afb7897191ab66c6403e6137
Arsenicmeister <98180323+Arsenicmeister@users.noreply.github.com>
2025-07-17 19:07:00 +0530

    Delete frontend/src/supabaseClient.js

--- frontend/src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-# Create a dummy supabaseClient file
-echo "// Dummy file - not used" > frontend/src/supabaseClient.js
-
-# Add to git
-git add frontend/src/supabaseClient.js
-git commit -m "Add dummy supabaseClient file"
-git push
